60e526cf816568a5f4f64778a9e5608563e884e8
hankli-0924 <hankli0924@gmail.com>
2025-01-05 14:24:31 +0800

    add db script

---
+++ tasks/db_code/veriii_defects.sql
@@ -0,0 +1,25 @@
+drop view if exists veriii_defects;
+create view veriii_defects as
+
+select content                                                                                             as issue_description,
+       "Executor"                                                                                          as owner,
+--        "Update Time",
+--        "Start Date",
+--        "Due Date",
+       问题类别                                                                                            as type,
+       系统名                                                                                              as sys_name,
+       模块                                                                                                as moudle_name,
+       "Priority"                                                                                          as priority,
+       case "Priority"
+           when '非常紧急' then 1
+           when '紧急' then 2
+           when '普通' then 3
+           when '较低'
+               then 4 end                                                                                  as priority_no,
+       "Workflow Status"                                                                                   as workflow_status,
+      to_date("Creation Time", 'yyyy-mm-dd')                                                                                    as creation_time,
+       extract(day from now() -
+                        to_timestamp("Creation Time", 'yyyy-mm-dd hh24:mi:ss'))                            as days_since_creation
+from all_veriii_defects
+where "Workflow Status" in ('Pending[待认领]', 'Working On It[工作进行中]')
+order by priority_no, creation_time
