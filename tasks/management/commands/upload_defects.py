05d5c1580b2e344bcd1b46ea336ee097342aba1d
hankli-0924 <hankli0924@gmail.com>
2025-01-09 06:56:17 +0800

    fix defects data loading issue

--- tasks/management/commands/upload_defects.py
+++ tasks/management/commands/upload_defects.py
@@ -1,7 +1,6 @@
-
 import pandas as pd
 from django.core.management.base import BaseCommand
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from django.conf import settings
 class Command(BaseCommand):
     help = 'Upload CSV data to the database'
@@ -25,6 +24,8 @@ class Command(BaseCommand):
         # Upload DataFrame to the database
         table_name = 'all_veriii_defects'  # 替换为实际表名
         with engine.begin() as connection:
+            # Truncate the table to remove all existing data
+            connection.execute(text(f'TRUNCATE TABLE "{table_name}" RESTART IDENTITY CASCADE'))
             df.to_sql(name=table_name, con=connection, if_exists='append', index=False, chunksize=1000)
 
         self.stdout.write(self.style.SUCCESS(f'Successfully uploaded {csv_file} to {table_name}'))
